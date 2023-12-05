b120e6f21125b83559bfaea2c2cd9964a3985532
eschalk <etienne.schalk@gmail.com>
2023-12-05 19:14:36 +0100

    Init Day 5

---
+++ advent_of_code/year_2023/year_2023_day_05.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+
+from advent_of_code.common import load_input_text_file
+
+
+@dataclass(frozen=True, kw_only=True)
+class Card:
+    identifier: int
+    winning_numbers: list[int]
+    numbers_you_have: list[int]
+
+
+def main():
+    result_part_1 = compute_part_1()
+    result_part_2 = compute_part_2()
+    print({1: result_part_1, 2: result_part_2})
+
+
+def compute_part_1():
+    data = parse_input_text_file()  # noqa: F841
+    ...
+    return None
+
+
+def compute_part_2():
+    data = parse_input_text_file()  # noqa: F841
+    ...
+    return None
+
+
+def parse_input_text_file() -> ...:
+    text = load_input_text_file(__file__)
+    parsed = parse_text_input(text)
+    return parsed
+
+
+def parse_text_input(text: str) -> ...:
+    lines = text.strip().split("\n")
+    ...
+    return lines
+
+
+if __name__ == "__main__":
+    main()
