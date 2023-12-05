f005beec6c2a49921bd8369cc2f23c5f2163e772
eschalk <etienne.schalk@gmail.com>
2023-12-05 19:55:21 +0100

    Day 5 initial parsing test

--- advent_of_code/year_2023/year_2023_day_05.py
+++ advent_of_code/year_2023/year_2023_day_05.py
@@ -3,11 +3,71 @@ from dataclasses import dataclass
 from advent_of_code.common import load_input_text_file
 
 
+@dataclass(frozen=True)
+class AlmanacRange:
+    destination_range_start: int
+    source_range_start: int
+    range_length: int
+
+    @property
+    def destination_range(self):
+        start = self.destination_range_start
+        return range(start, start + self.range_length)
+
+    @property
+    def source_range(self):
+        start = self.source_range_start
+        return range(start, start + self.range_length)
+
+    def is_source_in_source_range(self, source: int) -> bool:
+        return source in self.source_range
+
+    def source_to_target(self, source: int) -> int:
+        if self.is_source_in_source_range(source):
+            delta = source - self.source_range_start
+            return delta + self.destination_range_start
+        return source
+
+
 @dataclass(frozen=True, kw_only=True)
-class Card:
-    identifier: int
-    winning_numbers: list[int]
-    numbers_you_have: list[int]
+class AlmanacMap:
+    source_category: str
+    destination_category: str
+    ranges: list[AlmanacRange]
+
+    def source_to_target(self, source: int) -> int:
+        for range_ in self.ranges:
+            if range_.is_source_in_source_range(source):
+                return range_.source_to_target(source)
+        return source
+
+
+@dataclass(frozen=True, kw_only=True)
+class Almanac:
+    seeds: list[int]
+    maps: list[AlmanacMap]
+
+
+def parse_almanac_range(line: str) -> AlmanacRange:
+    return AlmanacRange(*(int(i) for i in line.split(" ")))
+
+
+def parse_almanac_map(lines: list[str]) -> AlmanacMap:
+    categories = lines[0].split(" ")[0].split("-")
+    source_category, destination_category = categories[0], categories[-1]
+    ranges = [parse_almanac_range(line) for line in lines[1:]]
+    return AlmanacMap(
+        source_category=source_category,
+        destination_category=destination_category,
+        ranges=ranges,
+    )
+
+
+def parse_almanac(text: str) -> Almanac:
+    sections = text.strip().split("\n\n")
+    seeds = [int(i) for i in sections[0].split(" ", 1)[-1].split(" ")]
+    maps = [parse_almanac_map(section.split("\n")) for section in sections[1:]]
+    return Almanac(seeds=seeds, maps=maps)
 
 
 def main():
--- tests/advent_of_code/year_2023/test_year_2023_day_05.py
+++ tests/advent_of_code/year_2023/test_year_2023_day_05.py
@@ -1,11 +1,89 @@
 from advent_of_code.year_2023.year_2023_day_00_template import parse_text_input
+from advent_of_code.year_2023.year_2023_day_05 import parse_almanac
 
 EXAMPLE_INPUT = """
 
-toto1
-toto2
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
 
 """
+
+
+def test_parse_almanac():
+    almanac = parse_almanac(EXAMPLE_INPUT)
+    assert almanac.seeds == [79, 14, 55, 13]
+
+    first_map = almanac.maps[0]
+
+    assert first_map.source_category == "seed"
+    assert first_map.destination_category == "soil"
+
+    assert first_map.ranges[0].destination_range_start == 50
+    assert first_map.ranges[0].source_range_start == 98
+    assert first_map.ranges[0].range_length == 2
+    assert first_map.ranges[0].destination_range == range(50, 50 + 2)
+    assert first_map.ranges[0].source_range == range(98, 98 + 2)
+    assert first_map.ranges[0].source_to_target(98) == 50
+    assert first_map.ranges[0].source_to_target(99) == 51
+
+    assert first_map.ranges[1].destination_range_start == 52
+    assert first_map.ranges[1].source_range_start == 50
+    assert first_map.ranges[1].range_length == 48
+    assert first_map.ranges[1].destination_range == range(52, 52 + 48)
+    assert first_map.ranges[1].source_range == range(50, 50 + 48)
+    assert first_map.ranges[1].source_to_target(53) == 55
+
+    assert first_map.source_to_target(98) == 50
+    assert first_map.source_to_target(99) == 51
+    assert first_map.source_to_target(53) == 55
+    assert first_map.source_to_target(10) == 10  # no mapping
+
+    assert first_map.source_to_target(0) == 0
+    assert first_map.source_to_target(1) == 1
+    assert first_map.source_to_target(48) == 48
+    assert first_map.source_to_target(49) == 49
+    assert first_map.source_to_target(50) == 52
+    assert first_map.source_to_target(51) == 53
+    assert first_map.source_to_target(96) == 98
+    assert first_map.source_to_target(97) == 99
+    assert first_map.source_to_target(98) == 50
+    assert first_map.source_to_target(99) == 51
+
+    assert first_map.source_to_target(79) == 81
+    assert first_map.source_to_target(14) == 14
+    assert first_map.source_to_target(55) == 57
+    assert first_map.source_to_target(13) == 13
 
 
 def test_year_2023_day_5_part_1():
