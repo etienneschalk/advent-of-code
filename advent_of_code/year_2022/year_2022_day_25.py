3d8a39694d596a1b52fd7a4a877864fc453b6003
eschalk <etienne.schalk@gmail.com>
2023-12-09 20:06:00 +0100

    Day 25 y 2022

--- advent_of_code/year_2022/year_2022_day_25.py
+++ advent_of_code/year_2022/year_2022_day_25.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from advent_of_code.common import load_input_text_file
 
 ProblemDataType = ...
@@ -6,19 +8,74 @@ ProblemDataType = ...
 def main():
     result_part_1 = compute_part_1()
     result_part_2 = compute_part_2()
+
     print({1: result_part_1, 2: result_part_2})
 
 
 def compute_part_1():
-    data = parse_input_text_file()
-    ...
-    return None
+    snafus = parse_input_text_file()
+    expected_decimal_sum = sum(convert_snafu_to_decimal(snafu) for snafu in snafus)
+    snafu_sum = convert_decimal_to_snafu(expected_decimal_sum)
+    return snafu_sum
 
 
 def compute_part_2():
-    data = parse_input_text_file()
-    ...
     return None
+
+
+def convert_snafu_to_decimal(snafu: str) -> int:
+    base = 5
+    components = []
+    for index, value in enumerate(reversed(snafu)):
+        if value == "-":
+            value = -1
+        elif value == "=":
+            value = -2
+        else:
+            value = int(value)
+        components.append(value * base**index)
+    return sum(components)
+
+
+def convert_decimal_to_snafu(decimal: int) -> str:
+    coefs = convert_decimal_to_snafu_ndarray(decimal)
+    mapping = {-2: "=", -1: "-", 0: "0", 1: "1", 2: "2"}
+    return "".join(mapping[coef] for coef in reversed(coefs)).lstrip("0")
+
+
+def convert_decimal_to_snafu_ndarray(decimal: int) -> str:
+    i = 0
+
+    while decimal % (5**i) != decimal:
+        i += 1
+
+    coefs = np.zeros(i + 1, dtype=np.int8)
+
+    remainder = decimal
+
+    while i > 0:
+        power = 5**i
+        quotient = remainder // power
+        remainder = remainder % power
+
+        if remainder > power // 2:
+            quotient += 1
+            remainder -= power
+
+        if quotient == -3:
+            coefs[i + 1] -= 1
+            quotient = 2
+        if quotient == 3:
+            coefs[i + 1] += 1
+            quotient = -2
+        if quotient == 4:
+            coefs[i + 1] += 1
+            quotient = -1
+        coefs[i] = quotient
+        i -= 1
+
+    coefs[0] = remainder
+    return coefs
 
 
 def parse_input_text_file() -> ProblemDataType:
