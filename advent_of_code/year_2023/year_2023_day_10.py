618b5bb7e7383d92bed83e7f36f90ded86935296
eschalk <etienne.schalk@gmail.com>
2023-12-10 13:59:03 +0100

    It works

--- advent_of_code/year_2023/year_2023_day_10.py
+++ advent_of_code/year_2023/year_2023_day_10.py
@@ -1,10 +1,90 @@
 import sys
+from pathlib import Path
 
 import numpy as np
+import xarray as xr
+from PIL import Image
+from skimage.morphology import flood_fill
 
 from advent_of_code.common import load_input_text_file
 
 ProblemDataType = np.ndarray
+
+DIR_B = np.array((1, 0))
+DIR_T = -DIR_B
+DIR_R = np.array((0, 1))
+DIR_L = -DIR_R
+DIR_TL = DIR_T + DIR_L
+DIR_TR = DIR_T + DIR_R
+DIR_BL = DIR_B + DIR_L
+DIR_BR = DIR_B + DIR_R
+
+PATTERNS = {
+    "│": np.bool_(
+        np.array(
+            [
+                [0, 1, 0],
+                [0, 1, 0],
+                [0, 1, 0],
+            ]
+        )
+    ),
+    "─": np.bool_(
+        np.array(
+            [
+                [0, 0, 0],
+                [1, 1, 1],
+                [0, 0, 0],
+            ]
+        )
+    ),
+    "└": np.bool_(
+        np.array(
+            [
+                [0, 1, 0],
+                [0, 1, 1],
+                [0, 0, 0],
+            ]
+        )
+    ),
+    "┘": np.bool_(
+        np.array(
+            [
+                [0, 1, 0],
+                [1, 1, 0],
+                [0, 0, 0],
+            ]
+        )
+    ),
+    "┐": np.bool_(
+        np.array(
+            [
+                [0, 0, 0],
+                [1, 1, 0],
+                [0, 1, 0],
+            ]
+        )
+    ),
+    "┌": np.bool_(
+        np.array(
+            [
+                [0, 0, 0],
+                [0, 1, 1],
+                [0, 1, 0],
+            ]
+        )
+    ),
+    "░": np.bool_(
+        np.array(
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+            ]
+        )
+    ),
+    # "S",
+}
 
 
 def main():
@@ -15,36 +95,105 @@ def main():
 
 def compute_part_1():
     maze = parse_input_text_file()
-    ...
-    print(render_pipes(maze))
-    result = logic_part_1(maze)
+    print(render_2d_array_to_text(maze))
+    result, minimum_distances = logic_part_1(maze)
     return result
 
 
 def compute_part_2():
-    data = parse_input_text_file()
+    maze = parse_input_text_file()
+    print(render_2d_array_to_text(maze))
+    result, minimum_distances = logic_part_1(maze)
+    arr = minimum_distances
+    main_loop = np.where(minimum_distances != 0, maze, "░")
+
+    i, j = locate_starting_index(main_loop)
+    neighbour_dict = get_neighbour_indices_dict(i, j)
+    allowed_pipes = get_allowed_pipes_for_direction()
+    directions = []
+    for direction, neighbour in neighbour_dict.items():
+        if main_loop[neighbour] in allowed_pipes[direction]:
+            directions.append(direction)
+
+    good_pipe = get_good_pipe(directions)
+
+    main_loop[i, j] = good_pipe
+    print(render_2d_array_to_text(main_loop))
+    arr_3x = np.zeros(3 * np.array(arr.shape), dtype=np.bool_)
+
+    # Ignore padded contour (1px)
+    for i in range(1, arr.shape[0] - 1):
+        for j in range(1, arr.shape[1] - 1):
+            fill_macro_pixel_3x(main_loop, arr_3x, i, j)
     ...
-    return None
+    result = arr_3x
+    grey = np.uint8(arr_3x) * 255
+    print(render_2d_array_to_text(grey))
+
+    save_img(arr_3x, Path("generated/arr_3x_1.png").resolve())
+    filled = flood_fill(
+        grey, (grey.shape[0] // 2, grey.shape[0] // 2), 128, tolerance=1
+    )
+
+    save_img(filled, Path("generated/arr_3x_2.png").resolve())
+    xda = xr.DataArray(filled, dims=("i", "j"))
+    coarsened = xda.coarsen(i=3, j=3).all()
+    coarsened_img = np.uint8(coarsened) * 255
+    save_img(coarsened_img, Path("generated/arr_3x_3.png").resolve())
+
+    result = np.sum(coarsened).item()
+
+    return result
+
+
+def save_img(arr_3x: np.ndarray, path: Path):
+    im = Image.fromarray(arr_3x)
+    im.save(path)
+    print(path)
+
+
+def get_good_pipe(directions):
+    if set(directions) == {"bottom", "top"}:
+        good_pipe = "│"
+    elif set(directions) == {"left", "right"}:
+        good_pipe = "─"
+    elif set(directions) == {"bottom", "right"}:
+        good_pipe = "┌"
+    elif set(directions) == {"bottom", "left"}:
+        good_pipe = "┐"
+    elif set(directions) == {"right", "top"}:
+        good_pipe = "└"
+    elif set(directions) == {"left", "top"}:
+        good_pipe = "┘"
+    return good_pipe
+
+
+# def compute_part_2():
+#     maze = parse_input_text_file()
+#     print(render_2d_array_to_text(maze))
+#     result, minimum_distances = logic_part_1(maze)
+#     arr = minimum_distances
+#     main_loop = np.where(minimum_distances != 0, maze, "░")
+#     print(render_2d_array_to_text(main_loop))
+#     arr_2x = np.zeros(2 * np.array(arr.shape), dtype=np.bool_)
+#     # Ignore padded contour (1px)
+#     for i in range(1, arr.shape[0] - 1):
+#         for j in range(1, arr.shape[1] - 1):
+#             fill_macro_pixel(main_loop, arr_2x, i, j)
+#     ...
+#     result = arr_2x
+#     print(render_2d_array_to_text(arr_2x))
+
+#     return result
 
 
 def logic_part_1(maze: ProblemDataType):
-    print("Current recursion limit:")
-    print(sys.getrecursionlimit())
-    sys.setrecursionlimit(15_000)
-    print("New recursion limit:")
-    print(sys.getrecursionlimit())
+    adapt_recursion_limit()
 
     i, j = locate_starting_index(maze)
-    neighbours = get_neighbour_indices(i, j)
-    neighbour_dict = dict(
-        zip(("top", "bottom", "left", "right"), get_neighbour_indices(i, j))
-    )
-    allowed_pipes = {
-        "top": {"│", "┐", "┌"},
-        "bottom": {"│", "┘", "└"},
-        "left": {"─", "└", "┌"},
-        "right": {"─", "┐", "┘"},
-    }
+    neighbour_dict = get_neighbour_indices_dict(i, j)
+    allowed_pipes = get_allowed_pipes_for_direction()
+
     values_dict = {}
     for direction, neighbour in neighbour_dict.items():
         if maze[neighbour] not in allowed_pipes[direction]:
@@ -58,8 +207,37 @@ def logic_part_1(maze: ProblemDataType):
         explore_maze(maze, values, distance, *neighbour)
         values_dict[neighbour] = values
     minimum_distances = np.minimum(*values_dict.values())
+    print(render_2d_array_to_text((minimum_distances != 0)))
+
     maximum = np.max(minimum_distances)
-    return maximum
+    return maximum, minimum_distances
+
+
+def get_allowed_pipes_for_direction():
+    allowed_pipes = {
+        "top": {"│", "┐", "┌"},
+        "bottom": {"│", "┘", "└"},
+        "left": {"─", "└", "┌"},
+        "right": {"─", "┐", "┘"},
+    }
+
+    return allowed_pipes
+
+
+def adapt_recursion_limit():
+    print("Current recursion limit:")
+    print(sys.getrecursionlimit())
+    sys.setrecursionlimit(15_000)
+    print("New recursion limit:")
+    print(sys.getrecursionlimit())
+
+
+def get_neighbour_indices_dict(i, j):
+    neighbour_dict = dict(
+        zip(("top", "bottom", "left", "right"), get_neighbour_indices(i, j))
+    )
+
+    return neighbour_dict
 
 
 def locate_starting_index(maze: ProblemDataType) -> tuple[int, int]:
@@ -101,6 +279,126 @@ def explore_maze(
             explore_maze(maze, values, distance, *candidate)
 
 
+def explore_maze_part_2(
+    maze: ProblemDataType,
+    maze_2x: ProblemDataType,
+    values: ProblemDataType,
+    distance: int,
+    i: int,
+    j: int,
+):
+    distance += 1
+    values[i, j] = distance
+
+    if maze[i, j] == "│":
+        candidates = ((i - 1, j), (i + 1, j))
+    elif maze[i, j] == "─":
+        candidates = ((i, j - 1), (i, j + 1))
+    elif maze[i, j] == "└":
+        candidates = ((i - 1, j), (i, j + 1))
+    elif maze[i, j] == "┘":
+        candidates = ((i - 1, j), (i, j - 1))
+    elif maze[i, j] == "┐":
+        candidates = ((i, j - 1), (i + 1, j))
+    elif maze[i, j] == "┌":
+        candidates = ((i, j + 1), (i + 1, j))
+    else:
+        return
+
+    for candidate in candidates:
+        # If a value is non-zero, the cell has already been explored
+        if not values[candidate]:
+            explore_maze_part_2(maze, values, distance, *candidate)
+
+
+def fill_macro_pixel_3x(
+    maze: ProblemDataType, maze_3x: ProblemDataType, i: int, j: int
+):
+    maze_3x[3 * i : 3 * i + 3, 3 * j : 3 * j + 3] = PATTERNS[maze[i, j]]
+    # for i in range(3):
+    #     for j in range(3):
+    #         maze_3x
+    # if maze[i, j] == "│":
+    #     candidates = ((i - 1, j), (i + 1, j))
+    # elif maze[i, j] == "─":
+    #     candidates = ((i, j - 1), (i, j + 1))
+    # elif maze[i, j] == "└":
+    #     candidates = ((i - 1, j), (i, j + 1))
+    # elif maze[i, j] == "┘":
+    #     candidates = ((i - 1, j), (i, j - 1))
+    # elif maze[i, j] == "┐":
+    #     candidates = ((i, j - 1), (i + 1, j))
+    # elif maze[i, j] == "┌":
+    #     candidates = ((i, j + 1), (i + 1, j))
+
+
+def fill_macro_pixel(maze: ProblemDataType, maze_2x: ProblemDataType, i: int, j: int):
+    # if maze[i, j] == "│":
+    #     candidates = ((i - 1, j), (i + 1, j))
+    # elif maze[i, j] == "─":
+    #     candidates = ((i, j - 1), (i, j + 1))
+    # elif maze[i, j] == "└":
+    #     candidates = ((i - 1, j), (i, j + 1))
+    # elif maze[i, j] == "┘":
+    #     candidates = ((i - 1, j), (i, j - 1))
+    # elif maze[i, j] == "┐":
+    #     candidates = ((i, j - 1), (i + 1, j))
+    # elif maze[i, j] == "┌":
+    #     candidates = ((i, j + 1), (i + 1, j))
+    n_dict = get_neighbour_indices_dict(i, j)
+    to = maze[n_dict["top"]] != "░"
+    bo = maze[n_dict["bottom"]] != "░"
+    le = maze[n_dict["left"]] != "░"
+    ri = maze[n_dict["right"]] != "░"
+    tl = to and le
+    tr = to and ri
+    bl = bo and le
+    br = bo and ri
+    tl_coord = (2 * i, 2 * j)
+    tr_coord = (2 * i, 2 * j + 1)
+    bl_coord = (2 * i + 1, 2 * j)
+    br_coord = (2 * i + 1, 2 * j + 1)
+    maze_2x[tl_coord] = tl
+    maze_2x[tr_coord] = tr
+    maze_2x[bl_coord] = bl
+    maze_2x[br_coord] = br
+
+
+# def fill_macro_pixel(maze: ProblemDataType, maze_2x: ProblemDataType, i: int, j: int):
+#     tl = maze[i, j]
+#     tr = maze[i, j + 1]
+#     bl = maze[i + 1, j]
+#     br = maze[i + 1, j + 1]
+#     tl_coord = (2 * i, 2 * j)
+#     tr_coord = (2 * i, 2 * j + 1)
+#     bl_coord = (2 * i + 1, 2 * j)
+#     br_coord = (2 * i + 1, 2 * j + 1)
+#     maze_2x[tl_coord] = tl
+#     maze_2x[tr_coord] = tr
+#     maze_2x[bl_coord] = bl
+#     maze_2x[br_coord] = br
+
+
+# def fill_macro_pixel(maze: ProblemDataType, maze_2x: ProblemDataType, i: int, j: int):
+#     n_dict = get_neighbour_indices_dict(i, j)
+#     t = maze[n_dict["top"]]
+#     b = maze[n_dict["bottom"]]
+#     l = maze[n_dict["left"]]
+#     r = maze[n_dict["right"]]
+#     tl = t and l
+#     tr = t and r
+#     bl = b and l
+#     br = b and r
+#     tl_coord = (2 * i, 2 * j)
+#     tr_coord = (2 * i, 2 * (j + 1))
+#     bl_coord = (2 * (i + 1), 2 * j)
+#     br_coord = (2 * (i + 1), 2 * (j + 1))
+#     maze_2x[tl_coord] = tl
+#     maze_2x[tr_coord] = tr
+#     maze_2x[bl_coord] = bl
+#     maze_2x[br_coord] = br
+
+
 def parse_input_text_file() -> ProblemDataType:
     text = load_input_text_file(__file__)
     parsed = parse_text_input(text)
@@ -132,11 +430,21 @@ def parse_text_input(text: str) -> ProblemDataType:
         text = text.replace(src, tgt)
     lines = text.strip().split("\n")
     input_array = np.array([np.array(list(line)) for line in lines])
-    return input_array
+
+    # Add a border of dots will ease later checks,
+    # not having to care about data outside the borders
+    padded_array = np.pad(input_array, pad_width=1, constant_values=b".")
+    return padded_array
 
 
-def render_pipes(data: ProblemDataType) -> str:
-    return "\n".join("".join(line) for line in data)
+def render_2d_array_to_text(data: ProblemDataType) -> str:
+    if data.dtype == np.bool_:
+        data = np.int8(data)
+        result = "\n".join("".join(str(c) for c in line) for line in data)
+        result = result.replace("0", "░").replace("1", "▓")
+    else:
+        result = "\n".join("".join(str(c) for c in line) for line in data)
+    return result
 
 
 if __name__ == "__main__":
--- tests/advent_of_code/year_2023/test_year_2023_day_10.py
+++ tests/advent_of_code/year_2023/test_year_2023_day_10.py
@@ -1,7 +1,10 @@
+import xarray as xr
+
 from advent_of_code.year_2023.year_2023_day_10 import (
+    fill_macro_pixel,
     logic_part_1,
     parse_text_input,
-    render_pipes,
+    render_2d_array_to_text,
 )
 
 EXAMPLE_INPUT_1_1 = """
@@ -65,25 +68,167 @@ def test_year_2023_day_10_part_1():
         EXAMPLE_INPUT_2_1,
         EXAMPLE_INPUT_2_2,
     )
-    expected = EXAMPLE_DISTANCES_1, EXAMPLE_DISTANCES_2
     parsed_inputs = [parse_text_input(test_input) for test_input in test_inputs]
     for p in parsed_inputs:
-        print(render_pipes(p))
+        print(render_2d_array_to_text(p))
 
-    result = logic_part_1(parsed_inputs[1])
+    result, minimum_distances = logic_part_1(parsed_inputs[1])
+    print(render_2d_array_to_text(minimum_distances))
     assert result == 4
 
-    result = logic_part_1(parsed_inputs[2])
+    result, minimum_distances = logic_part_1(parsed_inputs[2])
+    print(render_2d_array_to_text(minimum_distances))
     assert result == 4
 
-    result = logic_part_1(parsed_inputs[3])
-
+    result, minimum_distances = logic_part_1(parsed_inputs[3])
+    print(render_2d_array_to_text(minimum_distances))
     assert result == 8
-    result = logic_part_1(parsed_inputs[4])
+
+    result, minimum_distances = logic_part_1(parsed_inputs[4])
+    print(render_2d_array_to_text(minimum_distances))
     assert result == 8
     ...
 
 
+EXAMPLE_INPUT_PART_2_1_1a = """
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+"""
+
+EXAMPLE_INPUT_PART_2_1_1b = """
+...........
+.S-------7.
+.|F-----7|.
+.||OOOOO||.
+.||OOOOO||.
+.|L-7OF-J|.
+.|II|O|II|.
+.L--JOL--J.
+.....O.....
+"""
+EXAMPLE_INPUT_PART_2_1_2 = """
+..........
+.S------7.
+.|F----7|.
+.||OOOO||.
+.||OOOO||.
+.|L-7F-J|.
+.|II||II|.
+.L--JL--J.
+..........
+"""
+EXAMPLE_INPUT_PART_2_1_TEST = """
+..........
+.S------7.
+.|F----7|.
+.||....||.
+.||....||.
+.|L-7F-J|.
+.|..||..|.
+.L--JL--J.
+..........
+"""
+
+EXAMPLE_INPUT_PART_2_2_1 = """
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+"""
+
+EXAMPLE_INPUT_PART_2_2_2 = """
+OF----7F7F7F7F-7OOOO
+O|F--7||||||||FJOOOO
+O||OFJ||||||||L7OOOO
+FJL7L7LJLJ||LJIL-7OO
+L--JOL7IIILJS7F-7L7O
+OOOOF-JIIF7FJ|L7L7L7
+OOOOL7IF7||L7|IL7L7|
+OOOOO|FJLJ|FJ|F7|OLJ
+OOOOFJL-7O||O||||OOO
+OOOOL---JOLJOLJLJOOO
+"""
+
+EXAMPLE_INPUT_PART_2_3_1 = """
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+"""
+
+EXAMPLE_INPUT_PART_2_3_2 = """
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJIF7FJ-
+L---JF-JLJIIIIFJLJJ7
+|F|F-JF---7IIIL7L|7|
+|FFJF7L7F-JF7IIL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+"""
+
+
 def test_year_2023_day_10_part_2():
-    test_input = EXAMPLE_INPUT
-    parsed_input = parse_text_input(test_input)
+    test_inputs = (
+        EXAMPLE_INPUT_PART_2_1_1a,
+        EXAMPLE_INPUT_PART_2_1_1b,
+        EXAMPLE_INPUT_PART_2_1_2,
+        EXAMPLE_INPUT_PART_2_2_1,
+        EXAMPLE_INPUT_PART_2_2_2,
+        EXAMPLE_INPUT_PART_2_3_1,
+        EXAMPLE_INPUT_PART_2_3_2,
+    )
+
+    parsed_inputs = [parse_text_input(test_input) for test_input in test_inputs]
+    for p in parsed_inputs:
+        print(render_2d_array_to_text(p))
+        _, minimum_distances = logic_part_1(p)
+        print(render_2d_array_to_text(minimum_distances))
+
+    maze = parsed_inputs[2]
+    _, minimum_distances = logic_part_1(maze)
+    import numpy as np
+
+    contours = np.int32(np.bool_(minimum_distances))
+    rolled_i = np.roll(contours, 1, axis=0)
+    rolled_j = np.roll(contours, 1, axis=1)
+    print(render_2d_array_to_text(np.roll(contours, 1, axis=1)))
+    assert np.all(np.cumsum((contours - rolled_i), axis=0) == contours)
+    assert np.all(np.cumsum((contours - rolled_j), axis=1) == contours)
+
+    main_loop = np.where(minimum_distances != 0, maze, "░")
+    print(render_2d_array_to_text(main_loop))
+    arr = minimum_distances
+    arr_2x = np.zeros(2 * np.array(arr.shape), dtype=np.bool_)
+    # Ignore padded contour (1px)
+    for i in range(1, arr.shape[0] - 1):
+        for j in range(1, arr.shape[1] - 1):
+            fill_macro_pixel(main_loop, arr_2x, i, j)
+    ...
+    _ = arr_2x
+    print(render_2d_array_to_text(arr_2x))
+    xda = xr.DataArray(arr_2x, dims=("i", "j")).coarsen(i=2, j=2).sum()
+    print(render_2d_array_to_text(xda.data))
+
+    ...
