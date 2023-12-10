c2b1721cb11f219d0c49c9eac89b0ad37f2b68ee
eschalk <etienne.schalk@gmail.com>
2023-12-10 17:46:51 +0100

    Update visu

--- scripts/bunch_of_png_to_video.sh
+++ scripts/bunch_of_png_to_video.sh
@@ -3,7 +3,7 @@ OUTPUT_VID=$HOME/dev/advent-of-code/generated/advent_of_code/year_2023/day_10
 cd "${INPUT_PNG_DIR}"
 
 ffmpeg \
-    -framerate 2400 \
+    -framerate 5800 \
     -pattern_type glob -i '*.png' \
     -c:v libx264 \
     -pix_fmt yuv420p "${OUTPUT_VID}/out.mp4"
