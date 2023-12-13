c85181a0c1ddb8551a04b3997fc8165733b872ae
eschalk <etienne.schalk@gmail.com>
2023-12-13 23:46:59 +0100

    not the good answer

--- advent_of_code/year_2023/year_2023_day_13.py
+++ advent_of_code/year_2023/year_2023_day_13.py
@@ -19,20 +19,23 @@ def compute_part_1():
 
 
 def compute_part_2():
-    data = parse_input_text_file()
-    ...
-    return None
+    patterns = parse_input_text_file()
+    result = summarize_pattern_notes(patterns, smudge_mode=True)
+    return result
 
 
 def parse_input_text_file() -> ProblemDataType:
     text = load_input_text_file(__file__)
     parsed = parse_text_input(text)
+
+    # too low 32359
     return parsed
 
 
-def summarize_pattern_notes(patterns: ProblemDataType) -> int:
-    columns = tuple(find_number_of_cols_above_symmetry_axis(xda) for xda in patterns)
-    rows = tuple(find_number_of_rows_above_symmetry_axis(xda) for xda in patterns)
+def summarize_pattern_notes(
+    patterns: ProblemDataType, *, smudge_mode: bool = False
+) -> int:
+    columns, rows = compute_symmetry_amounts(patterns, smudge_mode=smudge_mode)
 
     number_of_columns = sum(columns)
     number_of_rows = sum(rows)
@@ -43,14 +46,74 @@ def summarize_pattern_notes(patterns: ProblemDataType) -> int:
     return summary
 
 
+def compute_symmetry_amounts(
+    patterns: ProblemDataType, *, smudge_mode: bool = False
+) -> tuple[tuple[int, ...], tuple[int, ...]]:
+    columns = tuple(
+        find_number_of_cols_above_symmetry_axis(xda, smudge_mode=smudge_mode)
+        for xda in patterns
+    )
+    rows = tuple(
+        find_number_of_rows_above_symmetry_axis(xda, smudge_mode=smudge_mode)
+        for xda in patterns
+    )
+
+    return columns, rows
+
+
 def find_number_of_cols_above_symmetry_axis(
     xda: xr.DataArray, *, smudge_mode: bool = False
 ) -> int:
-    return find_number_of_rows_above_symmetry_axis(xda.T, "col", "row")
+    if not smudge_mode:
+        return find_number_of_rows_above_symmetry_axis_part_1(xda.T, "col", "row")
+    else:
+        return find_number_of_rows_above_symmetry_axis_part_2(xda.T, "col", "row")
 
 
 def find_number_of_rows_above_symmetry_axis(
-    xda: xr.DataArray, row: str = "row", col: str = "col", *, smudge_mode: bool = False
+    xda: xr.DataArray, *, smudge_mode: bool = False
+) -> int:
+    if not smudge_mode:
+        return find_number_of_rows_above_symmetry_axis_part_1(xda)
+    else:
+        return find_number_of_rows_above_symmetry_axis_part_2(xda)
+
+
+def find_number_of_rows_above_symmetry_axis_part_2(
+    xda: xr.DataArray, row: str = "row", col: str = "col"
+) -> int:
+    # Impact of smudge:
+    # Start from the candidate symmetry line,
+    # and spread 1, then 2, etc. until a line is found (n)
+    # at n+1, there is PROBABLY a smudge.
+    # fixed smudge should have priority over non-smudge result
+    size = xda[row].size
+    candidates = [0]
+    for idx in range(1, size):
+        reflect_length = min(idx, size - idx)
+        current_xda = xda
+        for spread in range(1, reflect_length + 1):
+            left_lim = current_xda[idx - spread : idx]
+            right_lim = current_xda[idx : idx + spread][::-1]
+            upper_line = left_lim[0]
+            lower_line = right_lim[0]
+            if (upper_line != lower_line).sum() == 1:
+                if left_lim.size == 1 or right_lim.size == 1:
+                    candidates.append(idx)  # can already return
+                else:
+                    try_subarray = find_number_of_rows_above_symmetry_axis_part_1(
+                        current_xda.drop_sel({row: [idx - spread, idx + spread - 1]}),
+                        row,
+                        col,
+                    )
+                    if try_subarray > 0:
+                        candidates.append(idx)
+
+    return max(candidates)
+
+
+def find_number_of_rows_above_symmetry_axis_part_1(
+    xda: xr.DataArray, row: str = "row", col: str = "col"
 ) -> int:
     # Impact of smudge:
     # Start from the candidate symmetry line,
@@ -60,16 +123,64 @@ def find_number_of_rows_above_symmetry_axis(
     size = xda[row].size
     for idx in range(1, size):
         reflect_length = min(idx, size - idx)
-        for spread in (
-            range(1, reflect_length + 1) if smudge_mode else (reflect_length,)
-        ):
-            left = xda[:idx][-spread:].stack(z=(row, col), create_index=False)
-            right = xda[idx:][:spread][::-1].stack(z=(row, col), create_index=False)
-            identical_reflect = (left == right).all().item()
+        spread = reflect_length
+        left = xda[:idx][-spread:].stack(z=(row, col), create_index=False)
+        right = xda[idx:][:spread][::-1].stack(z=(row, col), create_index=False)
+        identical_reflect = (left == right).all().item()
 
-            if identical_reflect:
-                return idx
+        if identical_reflect:
+            return idx
     return 0
+
+
+# def find_number_of_rows_above_symmetry_axis(
+#     xda: xr.DataArray, row: str = "row", col: str = "col", *, smudge_mode: bool = False
+# ) -> int:
+#     # Impact of smudge:
+#     # Start from the candidate symmetry line,
+#     # and spread 1, then 2, etc. until a line is found (n)
+#     # at n+1, there is PROBABLY a smudge.
+#     # fixed smudge should have priority over non-smudge result
+#     size = xda[row].size
+#     for idx in range(1, size):
+#         reflect_length = min(idx, size - idx)
+#         repaired = False
+#         current_xda = xda
+#         for spread in (
+#             range(1, reflect_length + 1) if smudge_mode else (reflect_length,)
+#         ):
+#             # left_lim = xda[:idx][-spread:]
+#             # right_lim = xda[idx:][:spread][::-1]
+#             left_lim = current_xda[idx - spread : idx]
+#             right_lim = current_xda[idx : idx + spread][::-1]
+#             if smudge_mode:
+#                 upper_line = left_lim[0]
+#                 lower_line = right_lim[0]
+#                 if not repaired and (upper_line != lower_line).sum() == 1:
+#                     repaired = True
+#                     current_xda = current_xda.drop_sel(
+#                         {row: [idx - spread, idx + spread]}
+#                     )
+#                     if left_lim.size == 1 or right_lim.size == 1:
+#                         return idx  # can already return
+#             if repaired:
+#                 left_idx = idx - 1
+#                 right_idx = idx - 2
+#             else:
+#                 left_idx = right_idx = idx
+
+#             left_lim = current_xda[left_idx - spread : left_idx]
+#             right_lim = current_xda[right_idx : right_idx + spread][::-1]
+
+#             left = left_lim.stack(z=(row, col), create_index=False)
+#             right = right_lim.stack(z=(row, col), create_index=False)
+#             identical_reflect = (left == right).all()
+
+#             if identical_reflect and not (smudge_mode and not repaired):
+#                 return idx
+#         if repaired:
+#             idx += 2
+#     return 0
 
 
 # 24164 too low
--- tests/advent_of_code/year_2023/test_year_2023_day_13.py
+++ tests/advent_of_code/year_2023/test_year_2023_day_13.py
@@ -46,7 +46,6 @@ def test_year_2023_day_13_part_1_problematic():
     pattern = parse_text_input(test_input)[0]
 
     # Vertical symmetry
-    assert find_number_of_rows_above_symmetry_axis(pattern.T, "col", "row") == 10
     assert find_number_of_cols_above_symmetry_axis(pattern) == 10
 
     # Horizontal symmetry
@@ -95,13 +94,22 @@ def test_year_2023_day_13_part_2():
     print(render_2d_data_array(example_1))
     print(render_2d_data_array(example_2))
 
+    # columns, rows = compute_symmetry_amounts(patterns, smudge_mode=False)
+    # columns_s, rows_s = compute_symmetry_amounts(patterns, smudge_mode=True)
+    ...
     # Vertical symmetry
-    assert find_number_of_cols_above_symmetry_axis(example_1, smudge_mode=True) == 5
+    # Old symmetry disappeared
+    assert find_number_of_cols_above_symmetry_axis(example_1, smudge_mode=True) == 0
+    # No change, there was no symmetry
     assert find_number_of_cols_above_symmetry_axis(example_2, smudge_mode=True) == 0
 
     # Horizontal symmetry
-    assert find_number_of_rows_above_symmetry_axis(example_1, smudge_mode=True) == 0
-    assert find_number_of_rows_above_symmetry_axis(example_2, smudge_mode=True) == 4
+    # New expected symmetries
+    # 0 -> 3
+    assert find_number_of_rows_above_symmetry_axis(example_1, smudge_mode=True) == 3
+    # 4 -> 1
+    assert find_number_of_rows_above_symmetry_axis(example_2, smudge_mode=True) == 1
 
-    assert summarize_pattern_notes(patterns) == 405
+    # Change: 405 -> 400
+    assert summarize_pattern_notes(patterns, smudge_mode=True) == 400
     ...
