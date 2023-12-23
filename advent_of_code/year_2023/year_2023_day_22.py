7500d644d08716802f07f89b925a60952bf3327f
eschalk <etienne.schalk@gmail.com>
2023-12-23 15:46:37 +0100

    Part 1 OK

--- advent_of_code/year_2023/year_2023_day_22.py
+++ advent_of_code/year_2023/year_2023_day_22.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from dataclasses import dataclass
 
 import numpy as np
@@ -14,15 +15,7 @@ class Brick:
 
     @property
     def rank(self) -> int:
-        return min(coord[2] for coord in self.position)
-
-    @property
-    def lowest_z(self) -> int:
-        return min(coord[2] for coord in self.position)
-
-    @property
-    def highest(self) -> int:
-        return max(coord[2] for coord in self.position)
+        return min(self.z0, self.z1)
 
     @property
     def x0(self) -> int:
@@ -57,10 +50,6 @@ class Brick:
         return self.position[1]
 
     @property
-    def pos1(self) -> int:
-        return self.position[1]
-
-    @property
     def length(self) -> int:
         # The bricks span in only one direction
         # So all deltas will be null except for the brick's direction
@@ -69,7 +58,7 @@ class Brick:
 
     @property
     def height(self) -> int:
-        # +1 because the position tuple is incluseive
+        # +1 because the position tuple is inclusive
         return self.z1 - self.z0 + 1
 
     @property
@@ -90,10 +79,14 @@ class Brick:
         elif dx != 0:
             min_x = self.x0
             indexer = (slice(min_x, min_x + dx + 1), self.y0, self.z0)
-        else:
-            raise ValueError("Incorrect Brick")
+        else:  # pos0 and pos1 are equal = 1m3
+            indexer = tuple(self.pos0)
 
         return indexer
+
+    @property
+    def is_position_ordered(self) -> bool:
+        return np.all(self.pos1 >= self.pos0)
 
 
 ProblemDataType = list[Brick]
@@ -107,14 +100,150 @@ def main():
 
 def compute_part_1():
     data = parse_input_text_file()
-    ...
-    return None
+    result = compute_safely_removable_bricks(data)
+    return result
+    # {1: 471, 2: None}
+    # That's not the right answer; your answer is too high.
+    # Curiously, it's the right answer for someone else; you might be logged in
+    # to the wrong account or just unlucky. In any case,
+    # you need to be using your puzzle input. If you're stuck, make sure you're
+    # using the full input data; there are also some general tips on the about page,
+    # or you can ask for hints on the subreddit.
+    # Please wait one minute before trying again. [Return to Day 22]
 
 
 def compute_part_2():
     data = parse_input_text_file()
     ...
     return None
+
+
+def compute_safely_removable_bricks(unsorted_bricks: list[Brick]) -> int:
+    # Bricks that are the closest to the ground have the most priority (low-leaning z)
+    sorted_bricks = sorted(unsorted_bricks, key=lambda b: b.rank)
+
+    # The input is well-formed, no need to max min min max, positions are ordered.
+    assert np.all([sorted_brick.is_position_ordered for sorted_brick in sorted_bricks])
+
+    elevation_map = create_elevation_map(sorted_bricks)
+    fallen_bricks = compute_fallen_bricks(sorted_bricks, elevation_map)
+
+    space = create_space_datacuboid(fallen_bricks)
+    fill_space_with_bricks_identifiers(fallen_bricks, space)
+
+    supported_bricks = compute_supported_bricks(fallen_bricks, space)
+    support_counts = compute_support_counts(supported_bricks)
+    support_counts = dict(support_counts)
+    can_be_disintegrated = compute_disintegrable_bricks(
+        supported_bricks, support_counts
+    )
+    safely_removable_bricks = sum(can_be_disintegrated.values())
+
+    return safely_removable_bricks
+
+
+def compute_disintegrable_bricks(
+    supported_bricks: dict[int, tuple[int, ...]],
+    support_counts: dict[int, int],
+) -> dict[int, bool]:
+    # For the record: put an any instead of a all.
+    # Was producing the wrong output.
+    # Changed it to all, then it worked...
+    # When coding and inverting boolean logic on collections,
+    # ALWAYS double-check the quantifier!!!!
+    return {
+        brick_id: all(support_counts[sb_id] > 1 for sb_id in supported_bricks_ids)
+        or not supported_bricks_ids
+        for brick_id, supported_bricks_ids in supported_bricks.items()
+    }
+
+
+def compute_supported_bricks(
+    fallen_bricks: list[Brick], space: np.ndarray
+) -> dict[int, tuple[int, ...]]:
+    actual_supported: dict[int, tuple[int, ...]] = {}
+    for fallen_brick in fallen_bricks:
+        xx, yy, zz = fallen_brick.indexer
+        if isinstance(zz, slice):
+            new_zz = zz.stop
+        else:  # zz is an int
+            new_zz = zz + 1
+        just_above_indexer = (xx, yy, new_zz)
+        just_above = space[just_above_indexer]
+        supported = tuple(np.unique(just_above[just_above > 0]))
+        actual_supported[fallen_brick.identifier] = supported
+    return actual_supported
+
+
+def compute_support_counts(
+    supported_bricks: dict[int, tuple[int, ...]]
+) -> dict[int, int]:
+    # Unsafe when support_count == 1 (it means one support only)
+    # Note: bricks supported by the ground are not in the support counts
+    support_counts = defaultdict(int)
+    for brick_id, supported_bricks_ids in supported_bricks.items():
+        for supported_brick_id in supported_bricks_ids:
+            support_counts[supported_brick_id] += 1
+    return support_counts
+
+
+def fill_space_with_bricks_identifiers(
+    fallen_bricks: list[Brick], space: np.ndarray
+) -> None:
+    for fallen_brick in fallen_bricks:
+        space[fallen_brick.indexer] = fallen_brick.identifier
+
+
+def create_space_datacuboid(fallen_bricks: list[Brick]) -> np.ndarray:
+    xmax = max(max(b.x0, b.x1) for b in fallen_bricks)
+    ymax = max(max(b.y0, b.y1) for b in fallen_bricks)
+    zmax = max(max(b.z0, b.z1) for b in fallen_bricks)
+    # space_shape = (xmax + 1, ymax + 1, zmax + 1)
+    space_shape = (xmax + 1, ymax + 1, zmax + 2)
+    space = np.zeros(space_shape, dtype=int)
+    return space
+
+
+def compute_fallen_bricks(
+    sorted_bricks: list[Brick], elevation_map: np.ndarray
+) -> list[Brick]:
+    fallen_bricks = []
+    for idx, brick in enumerate(sorted_bricks, 1):
+        # elevation_map = (x,y)-2D array containing max(z)
+        # use elevation_map to know if the brick can still fall
+        # only use the x and y parts of the indexer for the elevation_map
+        fallen_brick = create_fallen_brick(elevation_map, idx, brick)
+        fallen_bricks.append(fallen_brick)
+
+    return fallen_bricks
+
+
+def create_fallen_brick(
+    elevation_map: np.ndarray, identifier: int, brick: Brick
+) -> Brick:
+    indexer = brick.indexer[:2]
+    peak = np.max(elevation_map[indexer])
+    new_peak = peak + brick.height
+    elevation_map[indexer] = new_peak
+    dz = brick.z1 - new_peak
+    dpos = np.array((0, 0, dz))
+    fallen_position = (brick.pos0 - dpos, brick.pos1 - dpos)
+    fallen_brick = Brick(
+        position=fallen_position,
+        falling=False,
+        identifier=identifier,
+    )
+
+    return fallen_brick
+
+
+def create_elevation_map(sorted_bricks: list[Brick]) -> np.ndarray:
+    xmax = max(max(b.x0, b.x1) for b in sorted_bricks)
+    ymax = max(max(b.y0, b.y1) for b in sorted_bricks)
+    elevation_map_shape = (xmax + 1, ymax + 1)
+    elevation_map = np.zeros(elevation_map_shape, dtype=int)
+
+    return elevation_map
 
 
 def parse_input_text_file() -> ProblemDataType:
--- tests/advent_of_code/year_2023/test_year_2023_day_22.py
+++ tests/advent_of_code/year_2023/test_year_2023_day_22.py
@@ -1,7 +1,16 @@
-from collections import defaultdict
 import numpy as np
 
-from advent_of_code.year_2023.year_2023_day_22 import Brick, parse_text_input
+from advent_of_code.year_2023.year_2023_day_22 import (
+    compute_disintegrable_bricks,
+    compute_fallen_bricks,
+    compute_safely_removable_bricks,
+    compute_support_counts,
+    compute_supported_bricks,
+    create_elevation_map,
+    create_space_datacuboid,
+    fill_space_with_bricks_identifiers,
+    parse_text_input,
+)
 
 EXAMPLE_INPUT = """
 
@@ -15,183 +24,95 @@ EXAMPLE_INPUT = """
 
 """
 
+EXPECTED_SPACE_PART_1 = np.array(
+    [
+        [
+            [0, 0, 2, 4, 0, 0, 0, 0],
+            [0, 0, 0, 4, 6, 0, 0, 0],
+            [0, 0, 3, 4, 0, 0, 0, 0],
+        ],
+        [
+            [0, 1, 2, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 6, 7, 7, 0],
+            [0, 1, 3, 0, 0, 0, 0, 0],
+        ],
+        [
+            [0, 0, 2, 5, 0, 0, 0, 0],
+            [0, 0, 0, 5, 6, 0, 0, 0],
+            [0, 0, 3, 5, 0, 0, 0, 0],
+        ],
+    ]
+)
 
-def test_year_2023_day_22_part_1():
+EXPECTED_SUPPORTED_BRICKS_PART_1 = {
+    1: (2, 3),
+    2: (4, 5),
+    3: (4, 5),
+    4: (6,),
+    5: (6,),
+    6: (7,),
+    7: (),
+}
+EXPECTED_SUPPORT_COUNTS_PART_1 = {2: 1, 3: 1, 4: 2, 5: 2, 6: 2, 7: 1}
+EXPECTED_CAN_BE_DISINTEGRATED_PART_1 = {
+    1: False,
+    2: True,
+    3: True,
+    4: True,
+    5: True,
+    6: False,
+    7: True,
+}
+
+
+def test_year_2023_day_22_part_1_whole():
+    test_input = EXAMPLE_INPUT
+    unsorted_bricks = parse_text_input(test_input)
+
+    result = compute_safely_removable_bricks(unsorted_bricks)
+
+    assert result == 5
+
+
+def test_year_2023_day_22_part_1_details():
     test_input = EXAMPLE_INPUT
     unsorted_bricks = parse_text_input(test_input)
 
     # Bricks that are the closest to the ground have the most priority (low-leaning z)
     sorted_bricks = sorted(unsorted_bricks, key=lambda b: b.rank)
 
-    # The input is nice, no need to max min min max, positions are ordered.
-    assert np.all(
-        [
-            np.all(sorted_brick.pos1 - sorted_brick.pos0 >= 0)
-            for sorted_brick in sorted_bricks
-        ]
-    )
+    # The input is well-formed, no need to max min min max, positions are ordered.
+    assert np.all([sorted_brick.is_position_ordered for sorted_brick in sorted_bricks])
 
-    xmax = max(max(b.x0, b.x1) for b in unsorted_bricks)
-    ymax = max(max(b.y0, b.y1) for b in unsorted_bricks)
-    zmax = max(max(b.z0, b.z1) for b in unsorted_bricks)
-    heatmap_shape = (xmax + 1, ymax + 1)
-    heatmap = np.zeros(heatmap_shape, dtype=int)
+    elevation_map = create_elevation_map(sorted_bricks)
 
-    # Create shadows of all bricks (projection along the z axis)
-    fallen_bricks = []
-    for idx, brick in enumerate(sorted_bricks, 1):
-        # shadow = footprint x and y wise (z = constant, the closest to the ground)
-        # use heatmap to know if the brick can still fall
+    fallen_bricks = compute_fallen_bricks(sorted_bricks, elevation_map)
 
-        # Only use the x and y parts of the indexer for the heatmap
-        indexer = brick.indexer[:2]
-        peak = np.max(heatmap[indexer])
-        new_peak = peak + brick.height
-        heatmap[indexer] = new_peak
-        dz = brick.z1 - new_peak
-        dpos = np.array((0, 0, dz))
-        fallen_position = (brick.pos0 - dpos, brick.pos1 - dpos)
-        fallen_brick = Brick(
-            position=fallen_position,
-            falling=False,
-            identifier=idx,
-        )
-        fallen_bricks.append(fallen_brick)
-    # priority_queue = queue.PriorityQueue()
-    # next_queue = queue.PriorityQueue()
-    # for b in sorted_bricks:
-    #     priority_queue.put(b)
+    space = create_space_datacuboid(fallen_bricks)
 
-    xmax = max(max(b.x0, b.x1) for b in fallen_bricks)
-    ymax = max(max(b.y0, b.y1) for b in fallen_bricks)
-    zmax = max(max(b.z0, b.z1) for b in fallen_bricks)
-    # space_shape = (xmax + 1, ymax + 1, zmax + 1)
-    space_shape = (xmax + 1, ymax + 1, zmax + 2)
-    space = np.zeros(space_shape, dtype=int)
-    for fallen_brick in fallen_bricks:
-        space[fallen_brick.indexer] = fallen_brick.identifier
-    assert np.all(
-        space
-        == np.array(
-            [
-                [
-                    [0, 0, 2, 4, 0, 0, 0, 0],
-                    [0, 0, 0, 4, 6, 0, 0, 0],
-                    [0, 0, 3, 4, 0, 0, 0, 0],
-                ],
-                [
-                    [0, 1, 2, 0, 0, 0, 0, 0],
-                    [0, 1, 0, 0, 6, 7, 7, 0],
-                    [0, 1, 3, 0, 0, 0, 0, 0],
-                ],
-                [
-                    [0, 0, 2, 5, 0, 0, 0, 0],
-                    [0, 0, 0, 5, 6, 0, 0, 0],
-                    [0, 0, 3, 5, 0, 0, 0, 0],
-                ],
-            ]
-        )
-    )
+    fill_space_with_bricks_identifiers(fallen_bricks, space)
 
-    # Write a function that compute this from space
-    expected_supported = {
-        1: (2, 3),
-        2: (4, 5),
-        3: (4, 5),
-        4: (6,),
-        5: (6,),
-        6: (7,),
-        7: (),
-    }
-    actual_supported: dict[int, tuple[int, ...]] = {}
-    for fallen_brick in fallen_bricks:
-        xx, yy, zz = fallen_brick.indexer
-        if isinstance(zz, slice):
-            # new_zz = slice(zz.start + 1, zz.stop + dz)
-            new_zz = zz.stop
-        else:  # zz is an int
-            new_zz = zz + 1
-        just_above_indexer = (xx, yy, new_zz)
-        just_above = space[just_above_indexer]
-        supported = tuple(np.unique(just_above[just_above > 0]))
-        actual_supported[fallen_brick.identifier] = supported
-        ...
-    assert actual_supported == expected_supported
-    expected_support_counts = defaultdict(int)
-    for brick_id, supported_bricks_ids in expected_supported.items():
-        for supported_brick_id in supported_bricks_ids:
-            expected_support_counts[supported_brick_id] += 1
+    assert np.all(space == EXPECTED_SPACE_PART_1)
+
+    supported_bricks = compute_supported_bricks(fallen_bricks, space)
+
+    assert supported_bricks == EXPECTED_SUPPORTED_BRICKS_PART_1
+
+    support_counts = compute_support_counts(supported_bricks)
+
     # Unsafe when support_count == 1 (it means one support only)
-    assert expected_support_counts == {2: 1, 3: 1, 4: 2, 5: 2, 6: 2, 7: 1}
     # Note: brick 1 (A) is not in the list, the ground supports it
-    ...
-    can_be_disintegrated = {
-        brick_id: any(
-            expected_support_counts[sb_id] > 1 for sb_id in supported_bricks_ids
-        )
-        or not supported_bricks_ids
-        for brick_id, supported_bricks_ids in expected_supported.items()
-    }
+    assert support_counts == EXPECTED_SUPPORT_COUNTS_PART_1
 
-    assert can_be_disintegrated == {
-        1: False,
-        2: True,
-        3: True,
-        4: True,
-        5: True,
-        6: False,
-        7: True,
-    }
+    can_be_disintegrated = compute_disintegrable_bricks(
+        supported_bricks, support_counts
+    )
 
-    # Done - now refactor
-    falling = [*sorted_bricks]
-    max_iter = 100
-    iter_count = 0
-    while falling and iter_count < max_iter:
-        iter_count += 1
-        brick: Brick = falling.pop(0)
+    assert can_be_disintegrated == EXPECTED_CAN_BE_DISINTEGRATED_PART_1
 
-        heatmap += brick.shadow * brick.height
+    safely_removable_bricks = sum(can_be_disintegrated.values())
 
-        # Intersect shadow at min z with heatmap
-        # If touch, update heatmap with max z
-        # and stop making this brick fall.
-
-        # continue falling until it cannot anymore
-
-    # max_simu_step = 50
-    # for i in range(max_simu_step):
-    #     max_iter = 100
-    #     iter_count = 0
-    #     while not priority_queue.empty() and iter_count < max_iter:
-    #         iter_count += 1
-    #         brick: Brick = priority_queue.get()
-
-    #         if brick.rank > 0:
-    #             brick.position[0] -= np.array([0, 0, 1])
-    #             brick.position[1] -= np.array([0, 0, 1])
-    #             next_queue.add(brick)
-    #         # TODO : after the brick is settled, fill the ground cube
-
-    #     priority_queue.clear()
-    #     priority_queue, next_queue = next_queue, priority_queue
-
-    ...
-    # First, run the simulation to make the bricks fall
-    # The simulation is settled when state(n+1) == state(n)
-    # "they won't go down any further"
-
-    # In this first phase it seams simpler to run the simul from the ground
-    # (make fall the closest bricks from the ground to make room for the ones above
-    # to fall too)
-
-    # In a second part, I assume some graph theory can be used
-    # "Touching" bricks make the graph connex
-    # If removing a brick make the graph not connex anymore, then it is dangerous
-    # to remove it as it means that bricks above will fall.
-    # "Touching" means that z2 = z1 + 1 (fall is vertical)
-    # Only the z axis connectivity seams interesting, at first.
-    ...
+    assert safely_removable_bricks == 5
 
 
 def test_year_2023_day_22_part_2():
