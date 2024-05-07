fa0f8b47a09be2dfd1584131a0b322f83bea6dc7
eschalk <etienne.schalk@gmail.com>
2024-05-07 10:52:14 +0200

    Improvement of day 24 nb

--- advent_of_code/visualization/observable_plot.py
+++ advent_of_code/visualization/observable_plot.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable
+from typing import Any, Callable, Literal
 
 import numpy as np
 import pandas as pd
@@ -8,18 +8,20 @@ from pyobsplot import Obsplot, Plot
 from advent_of_code.y_2023.problem_202311 import get_compartiments
 
 
-def create_obsplot_instance():
+def create_obsplot_instance(
+    *,
+    renderer: Literal["jsdom", "widget"] = "jsdom",
+    theme: Literal["current", "light", "dark"] = "dark",
+):
     # See https://juba.github.io/pyobsplot/usage.html#renderers
-    # This is a singleton instance of observable plot, allowing further customization like
-    # changing the renderer from 'widget' to 'json'.
 
-    op = Obsplot(renderer="jsdom", theme="current")  # Literal['current','light','dark']
-    # op = Obsplot(renderer="jsdom", theme="dark")
-    # op = Obsplot(renderer="widget")
-    return op
+    return Obsplot(renderer=renderer, theme=theme)
 
 
-op = create_obsplot_instance()
+# This is a singleton instance of observable plot, allowing further customization like
+# changing the renderer from 'widget' to 'json'.
+
+op = create_obsplot_instance(renderer="jsdom", theme="dark")
 
 
 def visualize_puzzle_input_202311(
@@ -136,6 +138,186 @@ def visualize_puzzle_input_202311(
     return build_base_xarray_plot(space_xda, callback, **kwargs)
 
 
+def visualize_puzzle_input_202324(
+    lines_df: pd.DataFrame,
+    *,
+    rock_df: pd.DataFrame | None = None,
+    width: int = 800,
+    strokeWidth: float = 2,
+    strokeOpacity: float = 1,
+    x_domain_test_area: tuple[float, float] = (7, 27),
+    y_domain_test_area: tuple[float, float] = (7, 27),
+    do_highlight_text: bool = False,
+    scheme: str = "Observable10",
+    rock_text_fill: str = "#faa",
+    rock_strokeWidth: float = 2,
+    rock_strokeDasharray: str = "3",
+    reverse_yx_axes: bool = True,
+    title: str | None = None,
+) -> Obsplot:
+    """Visualize the Puzzle Input of 202324.
+
+    This function draws 2D vectors that are stored in a ``DataFrame``
+
+    Parameters
+    ----------
+    lines_df
+        DataFrame with columns [x1 y1 x2 y2 time_in_ns]
+        representing a hailstone relative position vector
+    rock_df
+        DataFrame with columns [x1 y1 x2 y2 time_in_ns]
+        representing a rock relative position vector
+    width
+        Width of the produced plot
+    strokeWidth
+        Stroke width of hailstones' relative position vectors
+    strokeOpacity
+        Stroke opacity of hailstones' relative position vectors
+    x_domain_test_area
+        x-domain of the test area, materialized by a box in the plot
+    y_domain_test_area
+        x-domain of the test area, materialized by a box in the plot
+    do_highlight_text
+        De-clutter text mark by removing some of them. Useful for busy plots with many vectors
+    scheme
+        Categorical color scheme used to draw vectors. The colors depends of its time
+    rock_text_fill
+        Text Fill for the Text Mark used to annotate the time above the rock's trajectory
+    rock_strokeWidth
+        Stroke width of the rock's relative position vectors
+    rock_strokeDasharray
+        Stroke Dash Array pattern of the rock's relative position vectors
+    reverse_yx_axes
+        Flip the axis (useful to convert a portrait-plot to a landscape one, saving space)
+    title
+        Title of the plot
+
+    Returns
+    -------
+        An Observable Plot
+    """
+    if reverse_yx_axes:
+        x1_target = "y1"
+        x2_target = "y2"
+        y1_target = "x1"
+        y2_target = "x2"
+        x_target = "y"
+        y_target = "x"
+    else:
+        x1_target = "x1"
+        x2_target = "x2"
+        y1_target = "y1"
+        y2_target = "y2"
+        x_target = "x"
+        y_target = "y"
+
+    marks = [
+        Plot.link(  # type:ignore
+            lines_df,
+            {
+                x1_target: lines_df["x1"].to_list(),
+                y1_target: lines_df["y1"].to_list(),
+                x2_target: lines_df["x2"].to_list(),
+                y2_target: lines_df["y2"].to_list(),
+                "markerEnd": "arrow",
+                "curve": "linear",
+                "strokeWidth": strokeWidth,
+                "strokeOpacity": strokeOpacity,
+                "stroke": lines_df["time_in_ns"].to_list(),
+            },
+        ),
+        Plot.rect(  # type:ignore
+            pd.DataFrame.from_records(
+                {
+                    x1_target: [x_domain_test_area[0]],
+                    x2_target: [x_domain_test_area[1]],
+                    y1_target: [y_domain_test_area[0]],
+                    y2_target: [y_domain_test_area[1]],
+                }
+            ),
+            {
+                x1_target: "x1",
+                y1_target: "y1",
+                x2_target: "x2",
+                y2_target: "y2",
+                "stroke": "currentColor",
+            },
+        ),
+        Plot.text(  # type:ignore
+            lines_df,
+            {
+                x_target: lines_df["x2"].to_list(),
+                y_target: lines_df["y2"].to_list(),
+                "text": lines_df["time_in_ns"].to_list(),
+                "fill": "currentColor",
+                "stroke": "black",
+                "dy": -12,
+                # Only keep some text to avoid clutter
+                "filter": "highlight" if do_highlight_text else None,
+            },
+        ),
+    ]
+
+    if rock_df is not None:
+        marks.extend(
+            [
+                Plot.link(  # type:ignore
+                    rock_df,
+                    {
+                        x1_target: rock_df["x1"].to_list(),
+                        y1_target: rock_df["y1"].to_list(),
+                        x2_target: rock_df["x2"].to_list(),
+                        y2_target: rock_df["y2"].to_list(),
+                        "markerEnd": "arrow",
+                        "strokeOpacity": 1,
+                        "curve": "linear",
+                        "strokeWidth": rock_strokeWidth,
+                        "strokeDasharray": rock_strokeDasharray,
+                        # stroke=rock_df["time_in_ns"].to_list(),
+                        "stroke": "red",
+                        # headAngle=30,
+                    },
+                ),
+                Plot.text(  # type:ignore
+                    rock_df,
+                    {
+                        x_target: rock_df["x2"].to_list(),
+                        y_target: rock_df["y2"].to_list(),
+                        "text": rock_df["time_in_ns"].to_list(),
+                        "fill": rock_text_fill,
+                        "stroke": "black",
+                        "dy": -12,
+                    },
+                ),
+            ]
+        )
+
+    return op(  # type: ignore
+        dict(
+            grid=True,
+            x=dict(
+                label=x_target,
+                tickFormat=".0s",
+                nice=True,
+                # domain=list(x_domain),
+                ticks=10,
+            ),
+            y=dict(
+                label=y_target,
+                tickFormat=".0s",
+                nice=True,
+                # domain=list(y_domain),
+                ticks=10,
+            ),
+            color={"type": "categorical", "scheme": scheme},
+            marks=marks,
+            aspectRatio=1,
+            width=width,
+            title=title,
+        )
+    )
+
+
 def build_base_xarray_plot(
     xda: xr.DataArray,
     callback: Callable[[list[Any]], None],
@@ -144,8 +326,8 @@ def build_base_xarray_plot(
     scale: float = 1,
     width: int = 140 * 4,
     do_convert_ascii_array_to_uint8: bool = True,
-    **kwargs,
-):
+    **kwargs: Any,
+) -> Obsplot:
     # Callback is a consumer of list of marks that enrich it.
 
     grid = xda
