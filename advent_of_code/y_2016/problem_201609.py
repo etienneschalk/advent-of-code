4936d03cd92ccfd2e1c58d57c06e49aadb037406
eschalk <etienne.schalk@gmail.com>
2024-10-20 12:00:56 +0200

    part 2 201609

--- advent_of_code/y_2016/problem_201609.py
+++ advent_of_code/y_2016/problem_201609.py
@@ -18,18 +18,33 @@ class Instruction:
     width: int
     repeat: int
     span: tuple[int, int]
+    representation: str
 
     @classmethod
     def parse_list_from_text(cls, text: str) -> list[Self]:
         instructions = [
-            cls(
-                width=int(_match.group(2)),
-                repeat=int(_match.group(3)),
-                span=_match.span(),
-            )
+            cls.from_match_finditer(_match)
             for _match in re.finditer(r"(\((\d+)x(\d+)\))+?", text)
         ]
         return instructions
+
+    @classmethod
+    def from_match_finditer(cls, _match: re.Match) -> Self:
+        return cls(
+            width=int(_match.group(2)),
+            repeat=int(_match.group(3)),
+            span=_match.span(),
+            representation=_match.group(0),
+        )
+
+    @classmethod
+    def from_match_search(cls, _match: re.Match) -> Self:
+        return cls(
+            width=int(_match.group(3)),
+            repeat=int(_match.group(4)),
+            span=_match.span(),
+            representation=_match.group(2),
+        )
 
 
 @dataclass(kw_only=True)
@@ -74,7 +89,90 @@ class AdventOfCodeProblem201609(AdventOfCodeProblem[PuzzleInput]):
         return concat
 
     def solve_part_2(self, puzzle_input: PuzzleInput):
-        return -1
+        # Do not use the pre-parsed instruction, re-match them on the fly.
+        decompressed_length = self.solve_part_2_internal(puzzle_input.content)
+        return decompressed_length
+
+    def solve_part_2_internal(self, content: str, cursor: int = 0) -> int:
+        zone_end = len(content)
+
+        total: int = 0
+        while content:
+            # https://stackoverflow.com/questions/20236775/
+            # python-python-re-search-speed-optimization-for-long-string-lines
+            # res = re.search(r"(\((\d+)x(\d+)\))+?", content)
+            res = re.match(r"(.*?)(\((\d+)x(\d+)\))", content)
+            if res is None:
+                # Leaf case, return just the length of content
+                return zone_end
+            instr = Instruction.from_match_search(res)
+            # Note: because of the usage of match instead of find, the span includes the prefix.
+            data_segment_start = instr.span[1]
+            data_segment_end = data_segment_start + instr.width
+            prefix_len = data_segment_start - instr.span[0] - len(instr.representation)
+            data_segment = content[data_segment_start:data_segment_end]
+            recursive_result = self.solve_part_2_internal(data_segment)
+            total += prefix_len + instr.repeat * recursive_result
+            content = content[data_segment_end:]
+        return total
+        # total: int = 0
+        # cursor: int = 0
+        # instr_idx: int = 0
+        # instr = instructions[instr_idx]
+        # prefix_length = instr.span[0] - cursor
+        # start = instr.span[1]
+        # end = start + instr.width
+        # suffix = zone_end - cursor
+        # # Recursive but look ahead next instr to check if valid
+        # # to avoid looping when should not
+        # decoded_length = prefix_length + total * instr.repeat + suffix
+        # return decoded_length
+
+
+# def solve_part_2_internal(self, puzzle_input: PuzzleInput) -> str:
+#     instructions = puzzle_input.instructions
+#     content = puzzle_input.content
+
+#     cursor: int = 0
+#     decoded: list[str] = []
+#     instr_iter: Iterator[Instruction] = iter(instructions)
+#     instr = next(instr_iter, None)
+
+#     decoded_length, cursor = self.decode_recursive(
+#         puzzle_input, cursor, len(content), instr, instr_iter
+#     )
+#     # assert cursor == len(content)
+#     return decoded_length
+
+# def decode_recursive(
+#     self,
+#     puzzle_input: PuzzleInput,
+#     cursor: int,
+#     zone_end: int,
+#     instr: Instruction,
+#     instr_iter: Iterator[Instruction],
+# ) -> int:
+#     if instr is None:
+#         return zone_end - cursor, zone_end
+#     prefix_length = instr.span[0] - cursor
+#     start = instr.span[1]
+#     cursor = start + instr.width
+#     total = 0
+#     c = start
+#     print(
+#         f"Outer {instr=} {prefix_length=} {start=} {zone_end=} {cursor=} {total=} {c=}"
+#     )
+#     while c < cursor:
+#         instr = next(instr_iter, None)
+#         ll, c = self.decode_recursive(puzzle_input, c, cursor, instr, instr_iter)
+#         total += ll
+#         print(
+#             f"Inner {prefix_length=} {start=} {zone_end=} {cursor=} {total=} {c=} {ll=}"
+#         )
+
+#     suffix = zone_end - cursor
+#     decoded_length = prefix_length + total * instr.repeat + suffix
+#     return decoded_length, cursor
 
 
 if __name__ == "__main__":
--- tests/advent_of_code/y_2016/test_problem_201609.py
+++ tests/advent_of_code/y_2016/test_problem_201609.py
@@ -17,11 +17,20 @@ def test_problem_201609_part_1(example_inputs_2016: ExampleInputsStore):
     example_inputs = example_inputs_2016.retrieve(__file__, "example_inputs")
     example_inputs = ast.literal_eval(example_inputs)
     for input, expected_output in example_inputs.items():
-        print("------------")
         actual_output = problem.solve_part_1_internal(problem.parse_text_input(input))
-        print(input, expected_output, actual_output)
         assert actual_output == expected_output
 
 
 def test_problem_201609_part_2(example_inputs_2016: ExampleInputsStore):
-    pass
+    problem = AdventOfCodeProblem201609()
+    example_inputs = {
+        # "(3x3)XYZ": len("XYZXYZXYZ"),
+        # "X(8x2)(3x3)ABCY": len("XABCABCABCABCABCABCY"),
+        # "(27x12)(20x12)(13x14)(7x10)(1x12)A": 241920,
+        "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN": 445,
+    }
+    for input, expected_output in example_inputs.items():
+        print("------------")
+        actual_output = problem.solve_part_2_internal(problem.parse_text_input(input))
+        print(input, expected_output, actual_output)
+        assert actual_output == expected_output
