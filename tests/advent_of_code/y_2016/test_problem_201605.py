9b39c5160ea0f143efa89d858c3152d43a3fa1b7
eschalk <etienne.schalk@gmail.com>
2024-10-20 12:57:49 +0200

    Mark as slow

--- tests/advent_of_code/y_2016/test_problem_201605.py
+++ tests/advent_of_code/y_2016/test_problem_201605.py
@@ -3,18 +3,21 @@ import pytest
 from advent_of_code.y_2016.problem_201605 import AdventOfCodeProblem201605
 
 
+@pytest.mark.slow
 @pytest.mark.parametrize("puzzle_input,expected", [("abc", "18f47a30")])
 def test_problem_202205_part_1(puzzle_input: str, expected: str):
     result = AdventOfCodeProblem201605().solve_part_1(puzzle_input)
     assert result == expected
 
 
+@pytest.mark.slow
 @pytest.mark.parametrize("puzzle_input,expected", [("abc", "05ace8e3")])
 def test_problem_202205_part_2(puzzle_input: str, expected: str):
     result = AdventOfCodeProblem201605().solve_part_2(puzzle_input)
     assert result == expected
 
 
+@pytest.mark.slow
 @pytest.mark.parametrize("puzzle_input,expected", [("abbhdwsy", "801b56a7")])
 @pytest.mark.integration
 def test_problem_202205_part_1_integration(puzzle_input: str, expected: str):
@@ -22,6 +25,7 @@ def test_problem_202205_part_1_integration(puzzle_input: str, expected: str):
     assert result == expected
 
 
+@pytest.mark.slow
 @pytest.mark.parametrize("puzzle_input,expected", [("abbhdwsy", "424a0197")])
 @pytest.mark.integration
 def test_problem_202205_part_2_integration(puzzle_input: str, expected: str):
